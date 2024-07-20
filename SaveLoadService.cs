0bdd54c0e18a7dd5d179172b4e86b6820409362c
alihankvlc <147824938+alihankvlc@users.noreply.github.com>
2024-07-20 09:15:17 +0300

    Add files via upload

---
+++ SaveLoadService.cs
@@ -0,0 +1,27 @@
+﻿namespace Assets.KVLC
+{
+    public class SaveLoadService
+    {
+        private readonly SaveManager _saveManager;
+
+        public SaveLoadService(SaveManager saveManager)
+        {
+            _saveManager = saveManager;
+        }
+
+        public void SaveData<T>(T data) where T : ISaveable
+        {
+            _saveManager.Save(data);
+        }
+
+        public T LoadData<T>() where T : class, ISaveable
+        {
+            return _saveManager.Load<T>();
+        }
+
+        public bool ContainsKey<T>() where T : ISaveable
+        {
+            return _saveManager.ContainsKey<T>();
+        }
+    }
+}
---
+++ SaveManager.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Assets.KVLC
+{
+    public interface ISaveable { }
+
+    public class SaveManager
+    {
+        public void Save<T>(T data) where T : ISaveable
+        {
+            string json = JsonUtility.ToJson(data);
+            string filePath = GetFilePath<T>();
+
+            File.WriteAllText(filePath, json);
+        }
+
+        public T Load<T>() where T : class, ISaveable
+        {
+            string filePath = GetFilePath<T>();
+
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonUtility.FromJson<T>(json);
+            }
+
+            Debug.LogWarning($"File not found -> {filePath}");
+            return null;
+        }
+
+        public bool ContainsKey<T>() where T : ISaveable
+        {
+            string filePath = GetFilePath<T>();
+            return File.Exists(filePath);
+        }
+
+        private string GetFilePath<T>() where T : ISaveable
+        {
+            return Path.Combine(Application.persistentDataPath, $"{typeof(T).Name}.json");
+        }
+    }
+}
